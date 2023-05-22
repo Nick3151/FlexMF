883300bb43c934f108842c90a9df94403faa6393
Victor Xiang <xiangliang3151@gmail.com>
2023-05-22 11:26:42 -0400

    Simulation for choosing parameters in FlexMF

--- choose_lambda.m
+++ choose_lambda.m
@@ -3,53 +3,72 @@ close all
 clc
 root = fileparts(pwd);
 addpath(fullfile(root, 'TFOCS'))
+
 %% Generate some synthetic data
-number_of_seqences = 3;
-T = 3000; % length of data to generate
-Nneurons = 10*ones(number_of_seqences,1); % number of neurons in each sequence
+nSim = 100;
+rng(1)
+seeds = randperm(1000, nSim);
+
+number_of_seqences = 5;
+T = 5000; % length of data to generate
+Nneurons = 5*ones(number_of_seqences,1); % number of neurons in each sequence
 Dt = 3.*ones(number_of_seqences,1); % gap between each member of the sequence
-NeuronNoise = 0.001; % probability of added noise in each bin
-SeqNoiseTime = zeros(number_of_seqences,1); % Jitter parameter = 0%
-SeqNoiseNeuron = 1.*ones(number_of_seqences,1); % Participation parameter = 100%
-neg = 0; % Proportion of negative indices in W
-[X, W, H, V_hat] = generate_data(T,Nneurons,Dt,NeuronNoise,SeqNoiseTime,SeqNoiseNeuron,0,0,neg,1);
-figure; SimpleWHPlot(W,H,X); title('generated data','Fontsize',16)
-set(gcf,'position',[200,200,1200,900])
-nuc_norm = norm(svd(X),1);
-X = X/nuc_norm*size(X,1);
+noise = 0.001; % probability of added noise in each bin
+neg = 0;
+
+participation = 1.*ones(number_of_seqences,1); % Participation parameter = 100%
+warp = 2; % stretch should be less than Dt
+gap = 200;
 
 %% Procedure for choosing lambda
+
 nLambdas = 9; % increase if you're patient
 nAlphas = 9;
 K = 5; 
-L = 50;
 lambdas = sort([logspace(-1,-5,nLambdas)], 'ascend'); 
-alphas = sort([logspace(-1,-5,nAlphas)], 'ascend'); 
-loadings = zeros(nLambdas, nAlphas, K);
-reg_crosses = zeros(nLambdas, nAlphas);
-reg_Ws = zeros(nLambdas, nAlphas);
-reg_Hs = zeros(nLambdas, nAlphas);
-recon_errors = zeros(nLambdas, nAlphas); 
-times = zeros(nLambdas, nAlphas); 
-scores = zeros(nLambdas, nAlphas); 
-W_hats = cell(nLambdas, nAlphas);
-H_hats = cell(nLambdas, nAlphas);
+alphas = sort([logspace(-2,-6,nAlphas)], 'ascend'); 
+loadings = cell(nSim, nLambdas, nAlphas);
+reg_crosses = cell(nSim, nLambdas, nAlphas);
+reg_Ws = cell(nSim, nLambdas, nAlphas);
+reg_Hs = cell(nSim, nLambdas, nAlphas);
+recon_errors = cell(nSim, nLambdas, nAlphas);
+times = cell(nSim, nLambdas, nAlphas);
+scores = cell(nSim, nLambdas, nAlphas);
+num_detected = cell(nSim, nLambdas, nAlphas);
+W_hats = cell(nSim, nLambdas, nAlphas);
+H_hats = cell(nSim, nLambdas, nAlphas);
+Ws = cell(nSim, 1);
+Hs = cell(nSim, 1);
 
 for li = 1:length(lambdas)
     for ai = 1:length(alphas)
-        tic
-        [W_hat, H_hat, ~,loadings(li,ai,:),power]= FlexMF(X,'K',K,'L',L, 'maxiter', 50,...
-            'lambda', lambdas(li), 'alpha', alphas(ai), 'neg_prop', neg, 'showPlot', 0); 
-
-        [recon_errors(li,ai),reg_crosses(li,ai),reg_Ws(li,ai),reg_Hs(li,ai)] = helper.get_FlexMF_cost(X,W_hat,H_hat);
         display(['Testing lambda ' num2str(li) '/' num2str(length(lambdas))])
         display(['Testing alpha ' num2str(ai) '/' num2str(length(alphas))])
-        time = toc
-        times(li,ai) = time;
-        W_hats{li,ai} = W_hat;
-        H_hats{li,ai} = H_hat;
-        scores(li,ai) = helper.similarity(W, H, W_hat, H_hat);
+        lambda = lambdas(li);
+        alpha = alphas(ai);
+        parfor n=1:nSim
+            [X, W, H, ~] = generate_data(T,Nneurons,Dt, 'seed', seeds(n), 'gap', gap);
+            nuc_norm = norm(svd(X),1);
+            X = X/nuc_norm*size(X,1);
+            Ws{n} = W;
+            Hs{n} = H;
+            L = size(W,3);
+            tic
+            [W_hat, H_hat, ~, loadings{n,li,ai}, ~]= FlexMF(X,'K',K,'L',L, 'maxiter', 50,...
+                'lambda', lambda, 'alpha', alpha, 'neg_prop', neg, 'showPlot', 0, 'verbal', 0); 
+    
+            [recon_errors{n,li,ai},reg_crosses{n,li,ai},reg_Ws{n,li,ai},reg_Hs{n,li,ai}] = helper.get_FlexMF_cost(X,W_hat,H_hat);
+            
+            time = toc
+            times{n,li,ai} = time;
+            W_hats{n,li,ai} = W_hat;
+            H_hats{n,li,ai} = H_hat;
+            [coeff, ids] = helper.similarity_W(W, W_hat);
+            scores{n,li,ai} = coeff;
+            num_detected{n,li,ai} = length(ids);
+        end
     end
 end
+
 save('choose_lambda.mat', 'recon_errors', 'reg_crosses', 'reg_Ws', 'reg_Hs', 'times', 'loadings',...
-    'lambdas', 'alphas', 'W_hats', 'H_hats', 'scores')
+    'lambdas', 'alphas', 'W_hats', 'H_hats', 'scores', "num_detected", "Ws", "Hs")
--- choose_lambda_bayesopt.m
+++ choose_lambda_bayesopt.m
@@ -3,16 +3,26 @@ close all
 clc
 root = fileparts(pwd);
 addpath(fullfile(root, 'TFOCS'))
+
 %% Generate some synthetic data
-number_of_seqences = 3;
-T = 3000; % length of data to generate
-Nneurons = 10*ones(number_of_seqences,1); % number of neurons in each sequence
+number_of_seqences = 5;
+T = 5000; % length of data to generate
+Nneurons = 5*ones(number_of_seqences,1); % number of neurons in each sequence
 Dt = 3.*ones(number_of_seqences,1); % gap between each member of the sequence
-NeuronNoise = 0.001; % probability of added noise in each bin
-SeqNoiseTime = zeros(number_of_seqences,1); % Jitter parameter = 0%
-SeqNoiseNeuron = 1.*ones(number_of_seqences,1); % Participation parameter = 100%
-neg = 0; % Proportion of negative indices in W
-[X, W, H, V_hat] = generate_data(T,Nneurons,Dt,NeuronNoise,SeqNoiseTime,SeqNoiseNeuron,0,0,neg,1);
+noise = 0.001; % probability of added noise in each bin
+jitter = 5*ones(number_of_seqences,1); % Jitter std
+participation = 1.*ones(number_of_seqences,1); % Participation parameter = 100%
+warp = 2; % stretch should be less than Dt
+gap = 200;
+neg = 0;
+bin = 0;
+seed = 1;
+[X, W, H, ~] = generate_data(T,Nneurons,Dt, 'seed', seed, 'gap', gap);
+
+plotAll = 1;
+figure; SimpleWHPlot_patch(W,H,[],[],[],X,plotAll); title('generated data raw','Fontsize',16)
+set(gcf,'Units','normalized','Position',[0.1 0.1 0.8 0.8])
+
 nuc_norm = norm(svd(X),1);
 X = X/nuc_norm*size(X,1);
 
@@ -29,3 +39,14 @@ alpha_var = optimizableVariable('alpha', [1e-6, 1e-2], 'Transform', 'log');
 
 fun = @(x)compute_error_balance_score_2d(X,K,L,x.lambda,x.alpha);
 results = bayesopt(fun, [lambda_var,alpha_var],'AcquisitionFunctionName','expected-improvement-plus','UseParallel',true, 'MaxObjectiveEvaluations',100)
+
+%% Run with best parameters
+lambda = results.XAtMinEstimatedObjective.lambda;
+alpha = results.XAtMinEstimatedObjective.alpha;
+[W_hat,H_hat,cost,loadings,power] = FlexMF(X,'K',K, 'L', L, 'maxiter', 50,...
+    'lambda', lambda, 'alpha', alpha, 'lambdaL1W', 0, 'lambdaL1H', 0, 'neg_prop', 0, 'showPlot', 0);
+[recon_error, reg_cross, reg_W, reg_H] = helper.get_FlexMF_cost(X,W_hat,H_hat);
+
+plotAll = 1;
+figure; SimpleWHPlot_patch(W_hat,H_hat,[],[],[],[],plotAll); title('generated data raw','Fontsize',16)
+set(gcf,'Units','normalized','Position',[0.1 0.1 0.8 0.8])
